ee41025b10490be619e522126dcd9b9556dc03b2
Nicole Wang <nicole.wang@yale.edu>
2020-05-12 00:34:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,6 @@ def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
   array.map {|element| element ** 2 }
-  end
 end
 
 def summon_captain_planet(planeteer_calls)
